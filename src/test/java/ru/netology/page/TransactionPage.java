08ab733f1e2eed5be2c4c658d69bacd060bad5de
Lidiya <simutina.1994@mail.ru>
2023-05-05 21:50:19 +0300

    Initial commit

---
+++ src/test/java/ru/netology/page/TransactionPage.java
@@ -0,0 +1,28 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TransactionPage {
+    private SelenideElement sumAmount = $("[data-test-id='amount'] input");
+    private SelenideElement fromСard = $("[data-test-id='from'] input");
+    private SelenideElement replenish = $("[data-test-id='action-transfer']");
+    private SelenideElement transferHead = $(byText("Пополнение карты"));
+    private SelenideElement errorMessage = $("[data-test-id='error-message']");
+    private SelenideElement errorCard = $("[data-test-id='error-notification'] .notification__content");
+
+    public TransactionPage(){
+        transferHead.shouldBe(visible);
+    }
+
+    public DashboardPage transferOfMoneyValid(String amount, DataHelper.CardsInfo cardsInfo) {
+        sumAmount.setValue(amount);
+        fromСard.setValue(cardsInfo.getCardNumber());
+        replenish.click();
+        return new DashboardPage();
+    }
+}
---
+++ src/test/java/ru/netology/steps/TemplateSteps.java
@@ -0,0 +1,40 @@
+package ru.netology.steps;
+
+import com.codeborne.selenide.Selenide;
+import io.cucumber.java.ru.*;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+import ru.netology.page.TransactionPage;
+import ru.netology.page.VerificationPage;
+
+import static org.junit.Assert.assertEquals;
+import static ru.netology.data.DataHelper.getFirstCardNumber;
+import static ru.netology.data.DataHelper.getSecondCardNumber;
+
+public class TemplateSteps {
+    private static LoginPage loginPage;
+    private static DashboardPage dashboardPage;
+    private static VerificationPage verificationPage;
+    private static TransactionPage transactionPage;
+
+    @Пусть("пользователь залогинен с именем {string} и паролем {string}, вводит проверочный код 'из смс' {string}")
+    public void openAuthPage1(String login, String password, String verificationCode) {
+        loginPage = Selenide.open("http://localhost:9999", LoginPage.class);
+        verificationPage = loginPage.validLogin(login, password);
+        dashboardPage = verificationPage.validVerify(verificationCode);
+        dashboardPage.verifyIsDashboardPage();
+    }
+
+    @Когда("пользователь переводит {string} рублей с карты с номером 5559 0000 0000 0002 на свою 1 карту с главной страницы")
+    public void transferForFirstCard(int amount){
+        transactionPage = dashboardPage.transferMoney(getFirstCardNumber());
+        dashboardPage = transactionPage.transferOfMoneyValid(String.valueOf(amount),getSecondCardNumber());
+    }
+
+    @Тогда("баланс его 1 карты из списка на главной странице должен стать {string} рублей")
+    public void balanceForFirstCard(int expectedBalanceFirst, DataHelper.CardsInfo firstCard) {
+        var actualBalanceFirst = dashboardPage.getCardBalance(firstCard);
+        assertEquals(expectedBalanceFirst, actualBalanceFirst);
+    }
+}
